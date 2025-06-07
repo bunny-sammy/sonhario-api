2456c6f20ad44162d2f7022654ef834c05e78ede
bunnysammy_ <davidenacrene@gmail.com>
2025-06-07 14:09:10 -0300

    :wrench: WhiteNoise

--- sonhario/settings.py
+++ sonhario/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -136,6 +137,7 @@ STATIC_ROOT = "./static/"
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'sonhario/static'),
 ]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
