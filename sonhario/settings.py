c966c1e69a6e7b75145c3d5e73688bd48ee89e86
bunnysammy_ <davidenacrene@gmail.com>
2025-06-07 15:58:27 -0300

    :lipstick: feat: Landing page

--- sonhario/settings.py
+++ sonhario/settings.py
@@ -137,7 +137,8 @@ STATIC_ROOT = "./static/"
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'sonhario/static'),
 ]
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+if not DEBUG:
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
